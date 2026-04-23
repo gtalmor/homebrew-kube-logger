bd22493772aec91ac4f7a5b716028d4512b8252d
Gur Talmor <gtalmor@mac.com>
2026-04-23 07:08:49 +0200

    Bump kube-logger-agent to v0.2.4

--- Formula/kube-logger-agent.rb
+++ Formula/kube-logger-agent.rb
@@ -1,25 +1,25 @@
 class KubeLoggerAgent < Formula
   desc "Streams Kubernetes logs over WebSocket to the hosted Kube Logger viewer"
   homepage "https://github.com/gtalmor/Kube-Logger"
-  version "0.2.3"
+  version "0.2.4"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.3/kube-logger-agent-darwin-arm64"
-      sha256 "9cca36eb3f252f70d2241252a61849c282ecd550aaffb5eb1de9641b4e9a4c1a"
+      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.4/kube-logger-agent-darwin-arm64"
+      sha256 "586ce08ecdeb0edc2970727c07c3379ec9c85743cc6c118217fd92dea21605f0"
     else
-      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.3/kube-logger-agent-darwin-x64"
-      sha256 "0ef6dcca6fda79cfa537c1398de827468efe22be3f7e24bac82d347c9435aba4"
+      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.4/kube-logger-agent-darwin-x64"
+      sha256 "62643185ea47838c52ad6d845b5424b9f32ae140e812540bc6ac8aa96bd2c637"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
-      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.3/kube-logger-agent-linux-arm64"
-      sha256 "491a600cc035ee250765cabea956e1b87f9c13492b824ebfcaa5bf6fb4458852"
+      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.4/kube-logger-agent-linux-arm64"
+      sha256 "b8e4bf6e8c0b1467cca72142ad07b52c39ed9473627f3253af839a6310f464a2"
     else
-      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.3/kube-logger-agent-linux-x64"
-      sha256 "50a73120dc80c86112eff9ce0edbbdc5d6a824e63be9b3da0667e68552d3b693"
+      url "https://github.com/gtalmor/Kube-Logger/releases/download/v0.2.4/kube-logger-agent-linux-x64"
+      sha256 "72e645f20015b1025cf4cd76122e9514530bd18620c2458609cda325aa0a5158"
     end
   end
 
